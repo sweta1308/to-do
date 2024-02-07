6361fd8e6425112ae1e093218e69a36027c770b5
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-07 18:59:10 +0530

    add test for todo completed function

---
+++ src/utils/ToDoClassName.test.ts
@@ -0,0 +1,25 @@
+import { completedTodo } from './ToDoClassName'
+
+describe('completedTodo function', () => {
+  it('should return classNames with values when item status is completed', () => {
+    const item = { id: 1, todo: 'Sample Todo', status: 'completed' }
+    const expectedValues = {
+      listClassName: 'completed-item',
+      headClassName: 'head-line',
+      isCompleted: true,
+    }
+    const result = completedTodo(item)
+    expect(result).toEqual(expectedValues)
+  })
+
+  it('should return classNames with default values when item status is open', () => {
+    const item = { id: 1, todo: 'Sample Todo', status: 'open' }
+    const expectedClassNames = {
+      listClassName: '',
+      headClassName: '',
+      isCompleted: false,
+    }
+    const result = completedTodo(item)
+    expect(result).toEqual(expectedClassNames)
+  })
+})
