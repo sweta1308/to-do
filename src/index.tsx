2b3d87321171d5b36dce6ae62c201ce5f96631e9
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-07 14:57:49 +0530

    wrap provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ToDoProvider } from 'context/TodoContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
   <React.StrictMode>
-    <App />
+    <ToDoProvider>
+      <App />
+    </ToDoProvider>
   </React.StrictMode>,
 )
