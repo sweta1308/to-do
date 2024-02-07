3480dba8bffa65d959b4658a8c6ba583a1a2a2f3
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-07 17:08:14 +0530

    refactor: add handleFunctions

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -15,6 +15,20 @@ export const ToDoProvider: React.FC<ToDoProviderProps> = ({ children }) => {
   })
   const [toDo, setToDo] = useState<ToDoTypes[]>([])
   const [editId, setEditId] = useState<number>(0)
+
+  const openTodos = toDo.filter(
+    ({ status }: { status: string }) => status === 'open',
+  )
+  const completedTodos = toDo.filter(
+    ({ status }: { status: string }) => status === 'completed',
+  )
+
+  const handleAddClick = () =>
+    setInputState({ ...inputState, isInputVisible: true })
+
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) =>
+    setInputState({ ...inputState, inputValue: e.currentTarget.value })
+
   const handleSubmit = () => {
     if (inputState.inputValue) {
       if (editId > 0) {
@@ -38,13 +52,46 @@ export const ToDoProvider: React.FC<ToDoProviderProps> = ({ children }) => {
     }
   }
 
+  const handleCancelClick = () =>
+    setInputState({ inputValue: '', isInputVisible: false })
+
+  const handleCheckboxChange = (item: ToDoTypes) =>
+    setToDo((prev: ToDoTypes[]) =>
+      prev.map((todo) =>
+        todo.id === item.id
+          ? {
+              ...todo,
+              status: todo.status === 'open' ? 'completed' : 'open',
+            }
+          : todo,
+      ),
+    )
+
+  const handleEdit = (item: ToDoTypes) => {
+    setInputState({ inputValue: item.todo, isInputVisible: true })
+    setEditId(item.id)
+  }
+
+  const handleDelete = (item: ToDoTypes) => {
+    setToDo((prev: ToDoTypes[]) => prev.filter((todo) => todo.id !== item.id))
+    setInputState({ inputValue: '', isInputVisible: false })
+  }
+
+  const getToDoName = (name: string) =>
+    name.length > 40 ? name.substring(0, 40) + '...' : name
+
   const value = {
-    toDo,
-    setToDo,
     inputState,
-    setInputState,
+    openTodos,
+    completedTodos,
+    handleAddClick,
+    handleInputChange,
     handleSubmit,
-    setEditId,
+    handleCancelClick,
+    handleCheckboxChange,
+    handleEdit,
+    handleDelete,
+    getToDoName,
   }
   return <ToDoContext.Provider value={value}>{children}</ToDoContext.Provider>
 }
