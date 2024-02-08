55459bb2962975084745add561643d9b91594a26
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-08 17:37:21 +0530

    add drag drop context

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,17 @@
+import { DragDropContext } from 'react-beautiful-dnd'
 import './Home.css'
 import { Header, ToDoList } from 'components'
+import { useToDo } from 'context/TodoContext'
 
 const Home = () => {
+  const { dragEnded } = useToDo()
   return (
     <div className="home">
       <h1>To Do App</h1>
       <Header />
-      <ToDoList />
+      <DragDropContext onDragEnd={dragEnded}>
+        <ToDoList />
+      </DragDropContext>
     </div>
   )
 }
