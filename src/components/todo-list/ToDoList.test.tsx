dda30a6da1cfaa525c032ba849666ef4decb77df
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-08 10:37:40 +0530

    add tests for todo list

---
+++ src/components/todo-list/ToDoList.test.tsx
@@ -0,0 +1,63 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import ToDoList from './ToDoList'
+import { useToDo } from 'context/TodoContext'
+
+jest.mock('../../context/TodoContext', () => ({
+  useToDo: jest.fn(() => ({
+    openTodos: [
+      { id: 1, todo: 'Open Todo 1', status: 'open' },
+      { id: 2, todo: 'Open Todo 2', status: 'open' },
+    ],
+    completedTodos: [
+      { id: 3, todo: 'Completed Todo 1', status: 'completed' },
+      { id: 4, todo: 'Completed Todo 2', status: 'completed' },
+      { id: 5, todo: 'Completed Todo 3', status: 'completed' },
+    ],
+  })),
+}))
+
+describe('ToDoList Component', () => {
+  it('renders open and completed todos correctly', () => {
+    render(<ToDoList />)
+
+    expect(screen.getByText('Open Todos')).toBeInTheDocument()
+    expect(screen.getByText('Open Todo 1')).toBeInTheDocument()
+    expect(screen.getByText('Open Todo 2')).toBeInTheDocument()
+
+    expect(screen.getByText('Completed Todos')).toBeInTheDocument()
+    expect(screen.getByText('Completed Todo 1')).toBeInTheDocument()
+    expect(screen.getByText('Completed Todo 2')).toBeInTheDocument()
+    expect(screen.getByText('Completed Todo 3')).toBeInTheDocument()
+  })
+
+  it('displays "No Open Todos." message when there are no open todos', () => {
+    (useToDo as jest.Mock).mockReturnValue({
+      openTodos: [],
+      completedTodos: [
+        { id: 1, todo: 'Completed Todo 1', status: 'completed' },
+        { id: 2, todo: 'Completed Todo 2', status: 'completed' },
+        { id: 3, todo: 'Completed Todo 3', status: 'completed' },
+      ],
+    })
+
+    render(<ToDoList />)
+
+    expect(screen.getByText('No Open Todos.')).toBeInTheDocument()
+  })
+
+  it('displays "No Completed Todos." message when there are no completed todos', () => {
+    (useToDo as jest.Mock).mockReturnValue({
+      openTodos: [
+        { id: 1, todo: 'Open Todo 1', status: 'open' },
+        { id: 2, todo: 'Open Todo 2', status: 'open' },
+        { id: 3, todo: 'Open Todo 3', status: 'open' },
+      ],
+      completedTodos: [],
+    })
+
+    render(<ToDoList />)
+
+    expect(screen.getByText('No Completed Todos.')).toBeInTheDocument()
+  })
+})
