2b270e0fa2f06102a34c8b65f71e1824a5cea166
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-08 15:23:43 +0530

    add handleKeyDown event and inputRef

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,39 +1,39 @@
+import { useEffect } from 'react'
 import { useToDo } from '../../context/TodoContext'
 import './Header.css'
 
 const Header = () => {
   const {
-    inputState,
+    inputValue,
     handleSubmit,
     handleInputChange,
     handleCancelClick,
-    handleAddClick,
+    handleKeyDown,
+    inputRef,
   } = useToDo()
-  const { isInputVisible, inputValue } = inputState
+
+  useEffect(() => {
+    inputRef?.current?.focus()
+  }, [])
+
   return (
-    <div data-testid="header">
-      {isInputVisible ? (
-        <div className="inputs">
-          <input
-            placeholder="Enter Todo..."
-            className="text-input"
-            value={inputValue}
-            onChange={(e) => handleInputChange(e)}
-          />
-          <div>
-            <button onClick={handleSubmit} className="button">
-              Submit
-            </button>
-            <button onClick={handleCancelClick} className="cancel-btn">
-              Cancel
-            </button>
-          </div>
-        </div>
-      ) : (
-        <button onClick={handleAddClick} className="button">
-          Add Todo
+    <div className="inputs" data-testid="header">
+      <input
+        ref={inputRef}
+        placeholder="Enter Todo..."
+        className="text-input"
+        value={inputValue}
+        onChange={(e) => handleInputChange(e)}
+        onKeyDown={(e) => handleKeyDown(e)}
+      />
+      <div>
+        <button onClick={handleSubmit} className="button">
+          Submit
         </button>
-      )}
+        <button onClick={handleCancelClick} className="cancel-btn">
+          Cancel
+        </button>
+      </div>
     </div>
   )
 }
