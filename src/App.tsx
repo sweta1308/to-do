0470b0dc117175ddeed01ed2620b4a8f1a564829
Sweta Agarwalla <swetaagarwalla@Swetas-MacBook-Air.local>
2024-02-09 12:22:49 +0530

    add toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { Toaster } from 'react-hot-toast'
 import Home from 'pages/home/Home'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        containerStyle={{
+          top: '1rem',
+          right: '1rem',
+          fontSize: '0.9rem',
+        }}
+      />
       <Home />
     </div>
   )
